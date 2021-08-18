26ab14d57de3b677040c44a230b9bd0c375b5fa9
Erick Silva <erick.ap.silva@gmail.com>
2021-08-18 18:47:36 -0300

    Repository implementation

---
+++ src/main/java/one/digitalinnovation/personalapi/dto/response/MessageResponseDTO.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.personalapi.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageResponseDTO {
+
+    private String message;
+
+}
---
+++ src/main/java/one/digitalinnovation/personalapi/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.personalapi.repository;
+
+import one.digitalinnovation.personalapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+
+
+}
