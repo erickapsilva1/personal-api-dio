e4bb717fc2d4c2dfdeb48b8a95d778085a798c00
Erick Silva <erick.ap.silva@gmail.com>
2021-08-19 19:16:20 -0300

    Created readme.md file

--- src/main/java/one/digitalinnovation/personalapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personalapi/service/PersonService.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personalapi.service;
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personalapi.dto.request.PersonDTO;
 import one.digitalinnovation.personalapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personalapi.entity.Person;
@@ -14,16 +15,12 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
